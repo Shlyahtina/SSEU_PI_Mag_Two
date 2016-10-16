5bea590a2f32d36b65f10fb880aef263cf5cec51
Shlyahtina <25anfiska@mail.ru>
2016-10-16 14:53:41 +0400

    добавлена проверка для трех элементов

--- LabaTwo/LabaTwo/Program.cs
+++ LabaTwo/LabaTwo/Program.cs
@@ -10,18 +10,18 @@ namespace LabaTwo
     {
         static void Main(string[] args)
         {
-            int A, B;
-            if (int.TryParse(Console.ReadLine(), out A))
+            int A, B, C;
+            if (int.TryParse(Console.ReadLine(), out A) && int.TryParse(Console.ReadLine(), out B) && int.TryParse(Console.ReadLine(), out C))
             {
-                if (int.TryParse(Console.ReadLine(), out B))
-                {
+                //if (int.TryParse(Console.ReadLine(), out B))
+                //{
                     int max = (A >= B) ? A : B;
 
                     Console.WriteLine("Max = " + max);
-                }
-                else { Console.WriteLine("Невверное значение!Введите целое число."); };
+                //}
+                //else { Console.WriteLine("Невверное значение!Введите целое число."); };
             }
-            else { Console.WriteLine("Невверное значение!Введите целое число."); };
+            else { Console.WriteLine("Неверное значение! Введите целое число."); };
             Console.ReadKey();
         }
     }
