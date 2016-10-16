2edb34a73e239b1ed8665db88d4e789a79471aa3
Shlyahtina <25anfiska@mail.ru>
2016-10-16 14:40:33 +0400

    добавлено задание на нахождение максимума и минимума

---
+++ LabaTwo/LabaTwo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabaTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B;
+            if (int.TryParse(Console.ReadLine(), out A))
+            {
+                if (int.TryParse(Console.ReadLine(), out B))
+                {
+                    int max;
+                    if (A >= B)
+                    {
+                        max = A;
+                    }
+                    else
+                    {
+                        max = B;
+                    };
+
+                    Console.WriteLine("Max = " + max);
+                }
+                else { Console.WriteLine("Невверное значение!Введите целое число."); };
+            }
+            else { Console.WriteLine("Невверное значение!Введите целое число."); };
+            Console.ReadKey();
+        }
+    }
+}
