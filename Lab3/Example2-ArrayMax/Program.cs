ea8cb4cf48b6079132504dad385c8c3be21e505c
Shlyahtina <25anfiska@mail.ru>
2016-10-26 21:43:14 +0400

    Лаба 3. Пример 1. Нахождение максимума в массиве. обединение двех циклов в один

--- Lab3/Example2-ArrayMax/Program.cs
+++ Lab3/Example2-ArrayMax/Program.cs
@@ -17,11 +17,13 @@ namespace Example2_ArrayMax
             } while (!int.TryParse(Console.ReadLine(), out lengthArray) || (lengthArray <= 0));
             int[] array = new int[lengthArray];
             int i = 0;
+            int max = int.MinValue;
             while (i < lengthArray) 
             {
                 Console.Write("Введите array[" + i + "]: ");
                 if (int.TryParse(Console.ReadLine(), out array[i]))
                 {
+                    if (array[i] > max) { max = array[i]; };
                     i++; 
                 }
                 else 
@@ -29,11 +31,7 @@ namespace Example2_ArrayMax
                     Console.WriteLine("Введите целое число");
                 }; ;
             };
-            int max = array[0];
-            for (int k = 1; k < array.Length; k++) 
-            {
-                if (array[k] > max) { max = array[k]; };
-            };
+            
             Console.WriteLine("Максимально число "+max); 
             Console.ReadKey();
         }
