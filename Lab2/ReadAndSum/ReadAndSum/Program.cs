968a8df8b2df5467f36455aef97dc962e2696794
Shlyahtina <25anfiska@mail.ru>
2016-10-27 20:18:35 +0400

    В Lab2 перемещен Пример 1 - Сложение двух целых чисел

---
+++ Lab2/ReadAndSum/ReadAndSum/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne;
+            int numberTwo;
+            int summ;
+           
+                Console.WriteLine("Number one");
+                numberOne = int.Parse(Console.ReadLine());
+                Console.WriteLine("Number two");
+                numberTwo = int.Parse(Console.ReadLine());
+                summ = numberOne + numberTwo;
+                Console.WriteLine("Summ:" + summ);
+                Console.ReadKey();          
+        }
+    }
+}
