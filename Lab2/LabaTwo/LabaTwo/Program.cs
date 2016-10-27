037e432e91cb4ebee1e40cee5edd47b8472caefa
Shlyahtina <25anfiska@mail.ru>
2016-10-27 20:24:12 +0400

    перемещены все задачи относящиеся к лабе 2

---
+++ Lab2/LabaTwo/LabaTwo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabaTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C;            
+            if (int.TryParse(Console.ReadLine(), out A) && int.TryParse(Console.ReadLine(), out B) && int.TryParse(Console.ReadLine(), out C))
+            {
+                int max = ((max = (A >= B) ? A : B) >= C) ? max : C;
+                Console.WriteLine("Max = " + max);
+            }
+            else { Console.WriteLine("Неверное значение! Введите целое число."); };
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab2/Triangle/Triangle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c; // длины сторон треугольника
+
+            if (double.TryParse(Console.ReadLine(), out a) && double.TryParse(Console.ReadLine(), out b) && double.TryParse(Console.ReadLine(), out c))
+            {
+                // 1) существование треугольника
+                /** a<b+c
+                    a<c+a
+                    c<a+b*/
+
+                // 2) 
+
+            }
+            else { Console.WriteLine("Неверное значение! Введите число."); }
+            Console.ReadKey();
+        }
+    }
+}
