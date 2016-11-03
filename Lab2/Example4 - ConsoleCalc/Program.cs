2a93eb54b50dd323da44b99810ed683ae1c60af9
Shlyahtina <25anfiska@mail.ru>
2016-11-03 10:47:55 +0400

    Лаба 2. Пример 4. Консольный калькулятор: сначала вводим операнды, потом операцию

---
+++ Lab2/Example4 - ConsoleCalc/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example4___ConsoleCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double operand1 = 0;
+            double operand2 = 0;
+            double result = 0;
+            string operation;
+
+            Console.ReadKey();
+
+            if (double.TryParse(Console.ReadLine(), out operand1) && double.TryParse(Console.ReadLine(), out operand2))
+            {
+
+            }
+            else { Console.WriteLine("Операнд должен быть числом!"); };
+
+            //выбираем операцию
+            operation = Console.ReadLine();
+            switch (operation) 
+            {
+                case "+": result = operand1 + operand2; break;
+                case "-": result = operand1 - operand2; break;
+                case "*": result = operand1 * operand2; break;
+                case "/": result = operand1 / operand2; break;
+                case "sqrt": break;
+                default: Console.WriteLine("Неверная операция"); break;
+            };
+        }
+    }
+}
