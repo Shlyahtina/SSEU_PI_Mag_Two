0a6f5747efefb6b3b2d4818777d47d16275404ce
Shlyahtina <25anfiska@mail.ru>
2016-11-17 19:30:20 +0400

    test

---
+++ Lab4/Example1-FIO/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example1_FIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("¬ведите им€")ж
+        }
+    }
+}
