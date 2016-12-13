3f99e3d8e9b3d395d9af3ffa796fdc41fa55df43
Shlyahtina <25anfiska@mail.ru>
2016-12-13 22:52:49 +0400

    Л4. Задача 4 - Сжатия строки

---
+++ Lab4/Task4-Compression/Task4-Compression/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_Compression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string strIn = Console.ReadLine(), strRez = "";
+            char c = strIn[0];
+            strRez += c;
+            int len = 1;
+
+            for (int i = 1; i < strIn.Length; i++)
+                if (c == strIn[i])
+                    len++;
+                else
+                {
+                    c = strIn[i];
+                    strRez += string.Format("{0}{1}", len, c);
+                    len = 1;
+                }
+            strRez += string.Format("{0}", len);
+            if (strRez.Length > strIn.Length) strRez = strIn;
+            Console.WriteLine(strRez);
+            Console.ReadLine();
+        }
+    }
+}
