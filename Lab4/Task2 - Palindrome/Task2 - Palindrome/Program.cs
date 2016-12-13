43c96cdd0dd15346ca3b8eead6782398cf08e3b0
Shlyahtina <25anfiska@mail.ru>
2016-12-13 22:04:38 +0400

    Л4.Задача 2 - Палиндром

---
+++ Lab4/Task2 - Palindrome/Task2 - Palindrome/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2___Palindrome
+{
+    class Program
+    {
+
+            static void Main(string[] args)
+    {
+        Console.WriteLine("Введите строку: ");
+        string str = Console.ReadLine();
+        bool b = true;
+        str = str.Replace(" ",string.Empty).ToLower();
+
+        for(int i = 0; i < str.Length / 2; i++)
+        {
+            if (str[i] != str[str.Length - i - 1])
+                b = false;
+        }
+ 
+        Console.WriteLine(((b) ? "является" : "не является") + " палиндромом");
+        Console.ReadKey();
+        }
+    }
+}
