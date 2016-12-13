7e2e0aac644780de3e3b3954d03acdd9d48270cb
Shlyahtina <25anfiska@mail.ru>
2016-12-13 20:37:16 +0400

    Л4.задача 1 - Код Клавиши

---
+++ Lab4/Task1 - keyCode/Task1 - keyCode/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1___keyCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char key;
+            Console.WriteLine("Нажмите клавишу");
+            char.TryParse(Console.ReadLine(), out key);
+            int code;
+            code = Convert.ToInt32(key);
+            Console.WriteLine(code);
+            Console.ReadKey();
+            
+        }
+    }
+}
